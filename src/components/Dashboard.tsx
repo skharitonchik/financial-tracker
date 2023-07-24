52a3266922b5c1972d613e6c2053b8b9a54e74d0
Pavel <pavel.kovalevski111@gmail.com>
2023-07-24 22:50:16 +0300

    Dashboard updated

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,13 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
 import Box from '@mui/material/Box';
+
+import { TextField } from '@mui/material';
+import { Typography } from '@mui/material';
+import { Select } from '@mui/material';
+import { MenuItem } from '@mui/material';
+import { Converter } from './Converter';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -35,9 +41,54 @@ export const Dashboard: FC<DashboardProps> = () => {
     ],
   };
 
+  const [amount, setAmount] = useState(0);
+  const [firstCurrency, setFirstCurrency] = useState('BYN');
+  const [secondCurrency, setSecondCurrency] = useState('USD');
+
   return (
-    <Box sx={{ width: 500, height: 500 }}>
-      <Doughnut data={data} />
-    </Box>
+    <>
+      <Box sx={{ width: 500, height: 500 }}>
+        <Doughnut data={data} />
+      </Box>
+
+      <Box sx={{ mt: 1, height: 100}}>
+        <Box sx={{ mb: 2}}>
+          <Typography>Select currencies</Typography>
+          <Select
+            id='first-currency-selector'
+            onChange={(e) => {setFirstCurrency(e.target.value)}}
+            value={firstCurrency}
+            >
+            <MenuItem value={'BYN'}>BYN</MenuItem>
+            <MenuItem value={'USD'}>USD</MenuItem>
+            <MenuItem value={'EUR'}>EUR</MenuItem>
+          </Select>
+          <Select 
+            id='second-currency-selector'
+            onChange={(e) => {setSecondCurrency(e.target.value)}}
+            value={secondCurrency}
+            >
+            <MenuItem value={'BYN'}>BYN</MenuItem>
+            <MenuItem value={'USD'}>USD</MenuItem>
+            <MenuItem value={'EUR'}>EUR</MenuItem>
+          </Select>
+        </Box>
+
+        <TextField
+          sx={{mr: 1}}
+          placeholder='Input you amount'
+          label= {firstCurrency}
+          onChange={(e) => {setAmount(parseFloat(e.target.value))}}
+          type='number'
+          value={amount}>
+        </TextField>
+        
+        <Converter
+          amount={amount}
+          firstCurrency={firstCurrency}
+          secondCurrency={secondCurrency}
+          />
+      </Box>
+    </>
   );
 };
